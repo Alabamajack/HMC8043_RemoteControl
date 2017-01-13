6d2d7217814103ff3920b510d356cacd0e1c24ae
florian <flol2204+github@googlemail.com>
2017-01-13 12:50:37 +0100

    example of usage of using device

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from usbtmc import usbtmc
+#from usbtmc import usbtmc
 
-instr =  usbtmc.Instrument("USB::0x0AAD::0x0135::INSTR")
-print instr.ask("*IDN?")
+#instr =  usbtmc.Instrument("USB::0x0AAD::0x0135::INSTR")
+#print instr.ask("*IDN?")
+
+import commands
+
+hmc8043 = "/dev/usbtmc0"
+
+my_hmc = open(hmc8043, "rb+")
+
+my_hmc.write("INST OUT1\n")
+my_hmc.write("VOLT 10\n")
+my_hmc.write("INST OUT1\n")
+my_hmc.write("CURR 3\n")
+my_hmc.write("VOLT ?\n")
+print my_hmc.read(10)
+
+my_hmc.close()
